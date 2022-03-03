dfc6b40a33b0ab136a87126166672a49e6953d2a
KimberlyIn <51989121+KimberlyIn@users.noreply.github.com>
2022-03-03 17:47:37 +0800

    22/03/03

--- vue-week7/src/App.vue
+++ vue-week7/src/App.vue
@@ -7,6 +7,9 @@
 </template>
 
 <style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+// bootstrap icon
+@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
