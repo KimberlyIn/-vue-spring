aaaa7232b09528d4e8f1a42c7a4906996a206767
KimberlyIn <51989121+KimberlyIn@users.noreply.github.com>
2022-03-04 15:41:51 +0800

    22/03/04

--- vue-week7/src/App.vue
+++ vue-week7/src/App.vue
@@ -10,24 +10,4 @@
 @import '~bootstrap/scss/bootstrap';
 // bootstrap icon
 @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css');
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
