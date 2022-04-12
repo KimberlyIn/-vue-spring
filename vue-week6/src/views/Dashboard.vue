f4e983e0221e3446a3e227d7ccdf4924ea105f02
KimberlyIn <51989121+KimberlyIn@users.noreply.github.com>
2022-04-12 14:49:32 +0800

    22/04/12

--- vue-week6/src/views/Dashboard.vue
+++ vue-week6/src/views/Dashboard.vue
@@ -18,6 +18,19 @@
         </div>
       </div>
     </nav>
+    <!-- 
+    在這裡寫下這一段，我們說給他一個狀態，讓他決定是否把上面兩個 router link 呈現出來，
+    底下 data 我們放 checkSuccess: false 這是一個初始值，他的初始就是 false，因此代表一開始這一段是不會出現的，
+    那麼這一段不出現的結果就是，上面兩個 router link 也不會出現，這樣可以防範說，當你還沒有登入城東的時候，
+    當然是不會呈現畫面的。
+    
+    這邊做完以後，假如成功登入，底下的 checkLogin 就會馬上動作，確認我們是否登入。 
+    
+    當身份驗證成功之後，會看到裡面有個 this.checkSuccess = true; 這個時候就會呈現上面兩個 router link，
+    也就是我們登入成功了。
+
+    這樣的好處在於，不需要在每個頁面都寫上 checkLogin。
+    -->
     <router-view v-if="checkSuccess"></router-view>
   </div>
 </template>
